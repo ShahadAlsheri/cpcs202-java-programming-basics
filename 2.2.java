3c3f5000a2e9373a4addaa4a0e8b202ff49a2e33
Shahad <shahadalshehri05@gmail.com>
2026-04-20 18:31:18 +0300

    Update and rename ass3Q2.java to 2.2.java

--- 2.2.java
+++ 2.2.java
@@ -1,17 +1,4 @@
-
-package assignments;
-import java.util.Scanner;
-/*
-Course : CPCS 202
-Name : Shahad Alshehri
-University ID : 
-Section : DBR
-Name of lab instructor : 
-Problem number : 2
-Assignment number : 3
-Beecrowd id : 899919
-*/
-public class ass3Q2 {
+public class MainClass {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String a = input.nextLine();                
