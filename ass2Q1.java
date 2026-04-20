1b6252f2a593e4bc08504e2513a0df837b6c5aaf
Shahad <shahadalshehri05@gmail.com>
2026-04-20 18:14:17 +0300

    1.1.java

--- ass2Q1.java
+++ ass2Q1.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
-public class ass2Q1 {
+public class 1-2 {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         double x = input.nextDouble();
