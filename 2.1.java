3f610daca8dcf794ddf66d1ff1026040caa6739d
Shahad <shahadalshehri05@gmail.com>
2026-04-20 18:34:17 +0300

    Update 2.1.java

--- 2.1.java
+++ 2.1.java
@@ -2,7 +2,7 @@ public class MainCLass {
     public static void main(String[] args) {
         int i = 1;
         while (i <10){
-            int j;
+            int j; 
             for ( j = (i+6) ; j > (i+3); j--){
                 System.out.println("I=" + i + " J=" + j);
             }
